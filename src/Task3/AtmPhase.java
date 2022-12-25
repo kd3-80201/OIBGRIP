b7945a3bf49e731a56b3ea29d0043a90521764e5
Afnan Baig <mirzaafnanbaig@gmail.com>
2022-12-25 20:49:32 +0530

    3rd task added

---
+++ src/Task3/AtmPhase.java
@@ -0,0 +1,125 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class AtmPhase {
+    public static void main(String[] args) {
+
+
+            Connection conn;
+            Statement st;
+            ResultSet rs,rs2;
+            int u = 0,p = 0;
+            Scanner sc = new Scanner(System.in);
+            try {
+                // Connecting to MySQL Workbench
+                conn = DriverManager.getConnection("JDBC:mysql://localhost:3306/atm_interface", "root", "AfnanBaig@123");
+                st = conn.createStatement();
+            } catch (SQLException e) {throw new RuntimeException(e);}
+
+        while (true){
+            System.out.println("============================================================");
+            System.out.println("||                        Public ATM                      ||");
+            System.out.println("============================================================\n");
+
+            System.out.println("1. Login ");
+            System.out.println("2. Register ");
+            System.out.println("3. Quit\n");
+            System.out.println("Choose an Option ");
+
+
+            int input = sc.nextInt();
+            if (input == 1){
+                System.out.println("Enter Account No");
+                int ac = sc.nextInt();
+
+                System.out.println("Enter Pin");
+                int pn = sc.nextInt();
+
+                try {
+                    rs = st.executeQuery("select * from account where Account_No = '"+ac+"';"); // Getting values from the SQL Table
+                    while (rs.next()){
+                        u = rs.getInt("Account_No");
+                        p = rs.getInt("Pin");
+                    }
+                    if (ac == u && pn == p) { // if user is valid this condition will take him/her to the Bank Interface
+
+                        System.out.println("============================================================");
+                        System.out.println("||                 Welcome To Public Bank                 ||");
+                        System.out.println("============================================================\n");
+
+                        System.out.println("1. Transaction History");
+                        System.out.println("2. Withdraw");
+                        System.out.println("3. Deposit");
+                        System.out.println("4. Transfer");
+                        System.out.println("5. Check Balance");
+                        System.out.println("6. Quit\n");
+
+
+                        System.out.println("Choose an Option");
+
+                        int ch = sc.nextInt();
+                        face fc = new face();
+                        switch (ch) {
+                            case 1 -> fc.Transaction(u);
+                            case 2 -> fc.Withdraw(u);
+                            case 3 -> fc.Deposit(u);
+                            case 4 -> fc.Transfer(u);
+                            case 5 -> fc.Balance(u);
+                            case 6 -> System.exit(0);
+                            default -> System.out.println("You've Inputted the wrong option!");
+                        }
+                    }
+                    else {
+                        System.out.println("Wrong Credentials\n");
+                    }
+                } catch (SQLException e) {throw new RuntimeException(e);}
+
+            } else if (input == 2) { // New user will register here
+
+                Scanner sc1 = new Scanner(System.in);
+
+                System.out.println("Enter Your Name");
+                String na = sc1.nextLine();
+
+                System.out.println("Enter Your Age");
+                int age = Integer.parseInt(sc1.nextLine());
+
+                // I face the problem here while running this programme because in the
+                // nextLine() method it skips the input to be read, and takes the input of Age in place of Name.
+                // Hence,I did the syntax as Read the complete line as String and convert it to integer.
+
+                System.out.println("Enter Your Address");
+                String add = sc1.nextLine();
+
+
+                System.out.println("Enter Your Mobile No");
+                double num = sc1.nextDouble();
+
+                System.out.println("Enter Pin (In Digits)");
+                int pi = sc1.nextInt();
+
+                System.out.println("Enter Amount You want to Deposit");
+                double de = sc1.nextDouble();
+
+                int a;
+                try {
+                    rs2 = st.executeQuery("select * from account where Account_No = (select max(Account_No) from account);");
+                    rs2.next();
+                    a = (rs2.getInt("Account_No")) + 1;  // This query will take the last account number of user and add 1 to it
+
+                    //System.out.println(a);
+
+                    String save = "INSERT INTO account (Account_No,User_Name,Age,Address,Mobile_No,Pin,Balance) VALUES (" + a + ",'"+na+"',"+age+",'"+add+"',"+num+","+pi+","+de+");";
+                    st.executeUpdate(save); //
+
+                    System.out.println("Registration has completed ! \nAccount No of holder: "+a);
+                } catch (Exception e) {throw new RuntimeException(e);}
+
+                // now have to add the account number which is going to take as you have to take the previous account info and the add +1 to it
+
+            } else if (input == 3) {
+                System.exit(0);
+            }
+        }
+    }
+}
---
+++ src/Task3/face.java
@@ -0,0 +1,178 @@
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class face {
+Connection conn2;
+Statement st2,st3;
+ResultSet rs2,rs3;
+Scanner sc = new Scanner(System.in);
+
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    java.util.Date date = new Date();
+    String dt = dateFormat.format(date);
+
+    public face() {
+        try {
+            conn2 = DriverManager.getConnection("JDBC:mysql://localhost:3306/atm_interface", "root", "AfnanBaig@123");
+            st2 = conn2.createStatement();
+            st3 = conn2.createStatement();
+
+        } catch (SQLException e) {throw new RuntimeException(e);}
+
+    }
+    public void Transaction(int account){
+        System.out.println("====================================================================");
+        System.out.println("||                        Transaction History                     ||");
+        System.out.println("====================================================================\n");
+
+        try {
+            String history = "select * from transaction_table where Account_No = "+account+";";
+            rs2 = st2.executeQuery(history);
+            System.out.println(".......................................................................................................");
+            System.out.printf(" %-15s | %-15s | %-15s | %-16s | %-16s | %-16s  |","Account_No","Credit","Debit","Transaction Date","Credited To","Debited From");
+            System.out.println("\n.....................................................................................................");
+            while (rs2.next()){
+
+//                String a = rs2.getString(1);
+//                String b = rs2.getString(2);
+//                String c = rs2.getString(3);
+//                String d = rs2.getString(4);
+//                String e = rs2.getString(5);
+//                String f = rs2.getString(6);
+
+                String a = rs2.getString("Account_No");
+                String b = rs2.getString("Credit");
+                String c = rs2.getString("Debit");
+                String d = rs2.getString("Transaction_Date");
+                String e = rs2.getString("Credited_To");
+                String f = rs2.getString("Debited_From");
+
+
+                System.out.printf(" %-15s | %-15s | %-15s | %-16s | %-16s | %-16s  |\n",a,b,c,d,e,f);
+            }
+        } catch (SQLException e) {throw new RuntimeException(e);}
+
+
+    }
+
+
+    public void Withdraw(int account){
+
+        System.out.println("============================================================");
+        System.out.println("||                         Withdraw                       ||");
+        System.out.println("============================================================\n");
+        try {
+            rs2 = st2.executeQuery("select * from account where Account_No= "+account+";");
+            rs2.next();
+            String title = rs2.getString("User_Name");
+            double bal = rs2.getDouble("Balance"); // user balance will store here
+
+            System.out.println(" User Name: "+title);
+
+            System.out.println("Enter amount to withdraw");
+            int wt = sc.nextInt();
+
+            if (wt > bal){ // if the user has insufficient balance he can't withdraw cash
+                System.out.println("Sorry! Insufficient Balance.");
+            }
+            else {
+                st2.executeUpdate("update account set Balance = Balance - "+wt+";");
+
+                String gg = "Insert into transaction_table (Account_No,Debit,Transaction_Date) values ("+account+","+wt+",'"+dt+"');";
+                st3.executeUpdate(gg);
+                System.out.println("Amount is Withdrawn! Thank You For Using Public Bank");
+            }
+        } catch (SQLException e) {throw new RuntimeException(e);}
+
+    }
+    public void Deposit(int account){
+
+        System.out.println("============================================================");
+        System.out.println("||                          Deposit                       ||");
+        System.out.println("============================================================\n");
+
+        try {
+            rs2 = st2.executeQuery("select User_Name from account where Account_No= "+account+";");
+            rs2.next();
+            String title = rs2.getString("User_Name");
+            System.out.println(" User Name: "+title);
+
+            System.out.println("Enter Amount To Deposit");
+            int amt = sc.nextInt();
+
+            st2.executeUpdate("update account set Balance = Balance + "+amt+";");
+
+            String hh = "Insert into transaction_table (Account_No,Credit,Transaction_Date) values ("+account+","+amt+",'"+dt+"');";
+            st3.executeUpdate(hh);
+
+            System.out.println("Amount is Deposited! Thank You For Using Public Bank");
+        } catch (SQLException e) {throw new RuntimeException(e);}
+
+    }
+    public void Transfer(int account){
+
+        System.out.println("============================================================");
+        System.out.println("||                         Transfer                       ||");
+        System.out.println("============================================================\n");
+
+        System.out.println("Enter Recipient Account No");
+        int NumAccount = sc.nextInt();
+
+        try { // here transfer the values and compare them if there account number is correct or not and debit the money in transaction history and subtract the value in account's balance attribute
+
+            rs3 = st3.executeQuery("select Account_No from account where Account_No= "+NumAccount+";");
+            rs3.next();
+            boolean check = rs3.isBeforeFirst();
+
+            if (check){
+                rs2 = st2.executeQuery("select * from account where Account_No = "+NumAccount);
+                rs2.next();
+                String Compare = rs2.getString("User_Name");
+                double bal = rs2.getDouble("Balance");
+
+                System.out.println("Name of Recipient: "+Compare);
+
+                System.out.println("Enter Transfer Amount");
+                int TAmount = sc.nextInt();
+
+                if (TAmount > bal){
+                    System.out.println("Sorry! Insufficient Balance");
+                }
+                else {
+                    String ii = "insert into transaction_table (Account_No,Debit,Transaction_Date,Credited_To) values("+account+","+TAmount+",'"+dt+"',"+NumAccount+");"; // this side is from the sender who is transferring
+                    st3.executeUpdate("update account set Balance = Balance - "+TAmount+" where Account_No = "+account+";");
+                    // money to an account number, and you have to add an attribute as
+                    st2.executeUpdate(ii);
+
+                    String jj = "insert into transaction_table (Account_No,Credit,Transaction_Date,Debited_From) values ("+NumAccount+","+TAmount+",'"+dt+"',"+account+");"; // this amount is saved by the
+                    st2.executeUpdate(jj);
+
+                    System.out.println("Your Amount is transferred! Thank You For Using Public Bank ");
+                }
+            }
+            else {
+                System.out.println("Incorrect Account No!!!\n");
+            }
+
+        } catch (SQLException e) {throw new RuntimeException(e);}
+
+    }
+    public void Balance(int account){
+        try {
+
+            System.out.println("============================================================");
+            System.out.println("||                          Balance                       ||");
+            System.out.println("============================================================\n");
+            rs3 = st2.executeQuery("select Balance from account where Account_No = "+account+";");
+            rs3.next();
+            int showAmount  = rs3.getInt("Balance");
+            System.out.println("Bank Balance: "+showAmount);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
